5673eee68b4ee0cb9d0a81fde37a46af9b44904c
Tharun-Chennakki <channakkitharun2005@gmail.com>
2026-02-11 21:25:52 +0530

    Update Flask server to run on default host

--- drowsiness_backend/drowsiness.py
+++ drowsiness_backend/drowsiness.py
@@ -108,5 +108,5 @@ def detect():
 # --- START THE SERVER ---
 if __name__ == '__main__':
     print("🚀 Starting Flask server on http://127.0.0.1:5001")
-    app.run(host='0.0.0.0', port=5001)
+    app.run()
 
